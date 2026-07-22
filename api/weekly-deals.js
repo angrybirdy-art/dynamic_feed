7f80450376b56c994245187812185a47cb784321
angrybirdy-art <angrybirdy2003@gmail.com>
2026-07-22 16:42:51 +0530

    Add country information to weekly deals response

--- api/weekly-deals.js
+++ api/weekly-deals.js
@@ -3,9 +3,10 @@ export default function handler(req, res) {
 
   const discount = country === 'US' ? '50%' : '40%';
   const greeting = first_name ? `Hey ${first_name}, ` : '';
+  const countryText = country ? ` (${country})` : '';
 
   res.setHeader('Content-Type', 'text/html');
   res.status(200).send(
-    `<table><tr><td><h2>${greeting}This Week's Deals</h2><p>${discount} off all items...</p></td></tr></table>`
+    `<table><tr><td><h2>${greeting}This Week's Deals${countryText}</h2><p>${discount} off all items...</p></td></tr></table>`
   );
 }
